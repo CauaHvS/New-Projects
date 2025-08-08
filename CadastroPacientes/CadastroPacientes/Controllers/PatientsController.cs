74518b4e9c3e129b40bac73165d4e71fd656c261
Cauã Salgado <cauahenrique230503@gmail.com>
2025-08-08 01:41:42 -0300

    Projeto conectado ao banco

---
+++ CadastroPacientes/CadastroPacientes/Controllers/PatientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroPacientes.Controllers
+{
+    public class PatientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CadastroPacientes/CadastroPacientes/Models/Patients.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadastroPacientes.Models
+{
+    [Table("patients")]
+    public class Patients
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        public string CPF { get; set; }
+
+        [Required]
+        [StringLength(14)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
