c9a6c8572201146775f87796a84f8cd6787f50dd
Cauã Salgado <cauahenrique230503@gmail.com>
2025-08-08 09:04:12 -0300

    Remoçãos de comentarios

--- CadastroPacientes/CadastroPacientes/Controllers/PatientsController.cs
+++ CadastroPacientes/CadastroPacientes/Controllers/PatientsController.cs
@@ -19,13 +19,13 @@ namespace PatientRegistration.Controllers
             _context = context;
         }
 
-        // GET: Patients
+
         public async Task<IActionResult> Index()
         {
             return View(await _context.Patients.ToListAsync());
         }
 
-        // GET: Patients/Details/5
+
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -43,15 +43,12 @@ namespace PatientRegistration.Controllers
             return View(patients);
         }
 
-        // GET: Patients/Create
+
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Patients/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,DateOfBirth,CPF,PhoneNumber,Email")] Patients patients)
@@ -65,7 +62,7 @@ namespace PatientRegistration.Controllers
             return View(patients);
         }
 
-        // GET: Patients/Edit/5
+
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -81,9 +78,7 @@ namespace PatientRegistration.Controllers
             return View(patients);
         }
 
-        // POST: Patients/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DateOfBirth,CPF,PhoneNumber,Email")] Patients patients)
@@ -116,7 +111,7 @@ namespace PatientRegistration.Controllers
             return View(patients);
         }
 
-        // GET: Patients/Delete/5
+
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -134,7 +129,7 @@ namespace PatientRegistration.Controllers
             return View(patients);
         }
 
-        // POST: Patients/Delete/5
+
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
